c9b968174afde65e100f91bb585333a4dd1ef833
Roberta.Fiume <roberta.fiume@othermedia.com>
2019-10-08 17:08:39 +0100

    Creates interface with checkboxes and pass props to child component

--- src/App.js
+++ src/App.js
@@ -2,25 +2,58 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const learnString = " I learned to:"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ToDoComponent />
+        <WhatIlearned value = {learnString}/>
     </div>
   );
 }
+
+const ToDoComponent = () => {
+  return (
+    <div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 class="header-color">
+          Things to do in React: 
+        </h1>
+
+        <div class="ui checkbox">
+          <input type="checkbox" class="checkbox"  />
+          <label>Learn to use props</label>
+        </div>
+        <div class="ui checkbox">
+          <input type="checkbox" class="checkbox"  />
+          <label>Learn to use State</label>
+        </div>
+        <div class="ui checkbox">
+          <input type="checkbox" class="checkbox"  />
+          <label>Learn to Destructure things</label>
+        </div>
+        <div class="ui checkbox">
+          <input type="checkbox" class="checkbox"  />
+          <label>Make API calls</label>
+        </div>
+
+      
+      </header>
+    </div>
+  )
+}
+
+const WhatIlearned = (props) => {
+  console.log("THIS IS THE PROP", props);
+  return (
+    <div className="App-header">
+        <h2>{props.value}</h2>
+    </div>
+  )
+}
+
 
 export default App;
