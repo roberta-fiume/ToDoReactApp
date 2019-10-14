6472431c9cf07bfce0862f8d4181386e81a5e49e
Roberta.Fiume <roberta.fiume@othermedia.com>
2019-10-14 17:18:50 +0100

    adds style and adds crosses icons to deselect values

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import reactIcons from './react-icons.svg'
+import crossIcon from './cross.png'
 import { useState, useEffect } from 'react';
 import './App.css';
+
 
 const App = () => {
   return (
@@ -13,8 +16,7 @@ const App = () => {
 
 const Granparent = (props) => {
 
-  const learnString = " I learned to:";
-
+  const learnString = "I learned to:";
   const [checkboxstate, setCheckboxState] = useState({
     firstCheckbox: "",
     secondCheckbox:"",
@@ -24,68 +26,114 @@ const Granparent = (props) => {
 
   const [showCheckboxState, setShowCheckboxState] = useState(false);
 
+
+
+  const [showIconState, setIconState] = useState({
+    display: "none"
+  })
+
+
   const handleChange = event => {
     setCheckboxState({
       ...checkboxstate,
       [event.target.name]: event.target.value
     });
+    setIconState({
+      display:"flex"
+    })
+    console.log("THIS IS MY NEW STATE  FOR ICON", showIconState);
   };
 
-  console.log(checkboxstate);
   
- function printArray(event){
+ function changeCheckboxState(event){
     event.preventDefault();
-    console.log("THIS IS MY NEW STATE", checkboxstate);
     setShowCheckboxState(true);
-    console.log("THIS IS MY NEW STATE for showCheckboxState", showCheckboxState);
+    console.log("THIS IS THE ICON THAT BECOMES FLEX", showIconState)
   }
-  
-  return (
-  <div>
-    <h2>{props.value}</h2>
-    <form onSubmit = {printArray}>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="header-color">
-          Things to do in React: 
-        </h1>
 
-        <div className="ui checkbox">
-          <input type="checkbox"  name="firstCheckbox" type="checkbox" value={"Learn to use props"} onChange={handleChange}  />
+  return (
+  <div className="wrapper-form">
+    <h2 className="wrapper-title">{props.value}</h2>
+    <form onSubmit = {changeCheckboxState} className="form">
+
+     
+      <div className="form__img-wrapper">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={reactIcons} className="App-logo" alt="logo" />
+          <h1 className="header-color" className="form__header rainbow">
+            Things to do in React 
+          </h1>
+      </div>
+
+      <div className="form__input-wrapper">
+        <div className="ui checkbox" className="form__checkbox-box">
+          <input type="checkbox"  name="firstCheckbox" type="checkbox" value={"Learn to use props"} onChange={handleChange} className="checkbox" />
           <label>Learn to use props</label>
         </div>
-        <div className="ui checkbox">
+        <div className="form__checkbox-box">
           <input type="checkbox" name="secondCheckbox"  type="checkbox" value={"Learn to use state"}  onChange={handleChange} className="checkbox" />
           <label>Learn to use State</label>
         </div>
-        <div className="ui checkbox">
+        <div className="form__checkbox-box">
           <input type="checkbox"   name="third" type="checkbox" value={"Learn to use destructure things"} onChange={handleChange} className="checkbox"/>
           <label>Learn to Destructure things</label>
         </div>
-        <div className="ui checkbox">
+        <div className="form__checkbox-box">
           <input type="checkbox"  name="fourth" value={"Learn to use make API calls"} onChange={handleChange} className="checkbox"/>
           <label>Make API calls</label>
         </div>
-        <button>SUBMIT</button>
-      </header>
+      </div>
+        <button className="button">SUBMIT</button>
     </form>
     
-    <Parent value = {learnString} checkboxstate = {checkboxstate} showCheckboxState = {showCheckboxState}/>
+    <Parent value = {learnString} checkboxstate = {checkboxstate} showCheckboxState = {showCheckboxState} showIconState = {showIconState}/>
   
   </div>
   );
 }
-const Parent = ({ value, checkboxstate, showCheckboxState }) => { //prop destructuring
+const Parent = ({ value, checkboxstate, showCheckboxState, showIconState }) => { //prop destructuring
+
+  console.log("THIS IS MY CHECKBOX STATE IN PARENT",showIconState);
+
+  
+
+  
+
+  // function renderImage() {
+  //   console.log("I WORKKKK")
+  //   if (showIconState) {
+  //       return (
+  //         <img src={crossIcon} alt="logo"  className="cross"/>
+  //       );
+  //   }
+
+
   return (
-    <div className="App-header">
-      <h1>{value}</h1>
+    <div className="parent-wrapper">
+ 
       {showCheckboxState &&
         <div>
-          <p>{checkboxstate.firstCheckbox}</p>
-          <p>{checkboxstate.secondCheckbox}</p>
-          <p>{checkboxstate.third}</p>
-          <p>{checkboxstate.fourth}</p>
+          <div>
+              <h1 className="parent-title">{value}</h1>
+          </div>
+          <div className="parent__image-wrapper">
+              <img src={crossIcon}  style={showIconState} alt="logo"  className="cross"/>
+              <p className="parent-paragraph" >{checkboxstate.firstCheckbox}</p>
+          </div>
+          <div className="parent__image-wrapper"> 
+              <img src={crossIcon} style={showIconState} alt="logo" className="cross"/>
+              <p className="parent-paragraph" >{checkboxstate.secondCheckbox}</p>
+            </div>
+          <div className="parent__image-wrapper">
+              {/* <img src={crossIcon} alt="logo" className="cross"/> */}
+              <p className="parent-paragraph" >{checkboxstate.third}</p>
+          </div>
+          <div className="parent__image-wrapper">
+            {/* <img src={crossIcon} alt="logo" className="cross"/> */}
+            <p className="parent-paragraph">{checkboxstate.fourth}</p>
         </div>
+       </div>
+        
       }
     </div>
   )
